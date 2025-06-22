f06ca70937030ef08db227b08f701609c6010cdd
jinyoung <jink8962@naver.com>
2025-06-22 15:35:09 +0900

    [Bronze V] Title: 꼬마 정민, Time: 172 ms, Memory: 17664 KB -BaekjoonHub

---
+++ Java/백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner k = new Scanner(System.in);
+
+        long A = k.nextLong();
+        long B = k.nextLong();
+        long C = k.nextLong();
+
+        System.out.println(A + B + C);
+    }
+}
