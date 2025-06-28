3f570a64222443689b3831665f3fd5ecb0b75730
jinyoung <jink8962@naver.com>
2025-06-28 15:32:25 +0900

    Update OX퀴즈.java

--- Java/백준/Bronze/8958. OX퀴즈/OX퀴즈.java
+++ Java/백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -1,29 +1,25 @@
 import java.util.Scanner;
- 
+
 public class Main {
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		int T = sc.nextInt();
-		
-		for (int i = 0; i < T; i++) {
-			
-			String in = sc.next();
-			int sum = 0;
-			int score = 0;
-			
-			for (int j = 0; j < in.length(); j++) {
-				
-				if (in.charAt(j) == 'O') {
-					sum += 1;
-					score += sum;
-				}
-				
-				else
-					sum = 0;
-			}
-			System.out.println(score);
-		}
-	}
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        for (int i = 0; i < T; i++) {
+            String in = sc.next();
+            int sum = 0;
+            int score = 0;
+
+            for (int j = 0; j < in.length(); j++) {
+                if (in.charAt(j) == 'O') {
+                    sum += 1;
+                    score += sum;
+                } else {
+                    sum = 0;
+                }
+            }
+            System.out.println(score);
+        }
+    }
 }
