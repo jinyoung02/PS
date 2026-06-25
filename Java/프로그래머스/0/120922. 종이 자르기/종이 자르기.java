ed4f6918e69b12c6811cbc4b19c0e36abb764a92
JinYoung Kim <jink8962@naver.com>
2026-06-25 20:02:10 +0900

    [level 0] Title: 종이 자르기, Time: 0.02 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120922. 종이 자르기/종이 자르기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int M, int N) {
+        int answer = 0;
+        
+        int k = M * N;
+        
+        answer = k - 1;
+        
+        return answer;
+    }
+}
