6b92d85e4b01ef3273d64b45179fe1b8c403940d
JinYoung Kim <jink8962@naver.com>
2026-07-06 19:35:52 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 3.85 ms, Memory: 74.6 MB -BaekjoonHub

--- Java/프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ Java/프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -6,6 +6,7 @@ class Solution {
         for(int i=0; i<seoul.length; i++) {
             if(seoul[i].equals(kim)) {
                 answer = "김서방은 " + i + "에 있다";
+                break;
             }
         }
 
