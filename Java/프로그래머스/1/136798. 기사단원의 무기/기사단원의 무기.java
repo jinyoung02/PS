b000f25063b7610a2187f33761e7be7f3acd0110
JinYoung Kim <jink8962@naver.com>
2026-07-08 12:02:53 +0900

    [level 1] Title: 기사단원의 무기, Time: 6.22 ms, Memory: 80.6 MB -BaekjoonHub

--- Java/프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ Java/프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,14 +1,44 @@
 class Solution {
     public int solution(int number, int limit, int power) {
+        int[] count = new int[number + 1];
+        
+        // i의 배수들은 모두 i를 약수로 가짐
+        for(int i=1; i<=number; i++) {
+            for(int j=1; j<=number/i; j++) {
+                count[i * j]++;
+            }
+        }
+
+        int answer = 0;
+
+        for(int i=1; i<=number; i++) {
+            if(count[i] > limit) {
+                answer += power;
+            } else {
+                answer += count[i];
+            }
+        }
+
+        return answer;
+    }
+}
+
+/*
+처음에 푼 풀이
+
+class Solution {
+    public int solution(int number, int limit, int power) {
         int[] p = new int[number]; // 약수의 개수 저장
         int s = 0;
         
         for(int i=1; i<=number; i++) {
             int count = 0;
+
             for(int j=1; j<=i; j++) {
                 if(i % j == 0) {
                     count += 1;
                 }
+
                 p[i-1] = count;
             }
         }
@@ -16,8 +46,7 @@ class Solution {
         for(int a=0; a<p.length; a++) {
             if(p[a]<=limit) {
                 s = s + p[a];
-            }
-            else {
+            } else {
                 s = s + power;
             }
         }
@@ -25,3 +54,4 @@ class Solution {
         return s;
     }
 }
+*/
