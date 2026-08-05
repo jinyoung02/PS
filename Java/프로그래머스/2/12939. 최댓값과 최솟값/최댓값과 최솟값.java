6f7e4ad5405ec77d55e310dc407ca3ea8f3f93f4
JinYoung Kim <jink8962@naver.com>
2026-08-05 13:12:24 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 3.72 ms, Memory: 83.4 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String solution(String s) {
+        String[] arr = s.split(" ");
+
+        int min = Integer.parseInt(arr[0]);
+        int max = Integer.parseInt(arr[0]);
+
+        for(int i=1; i<arr.length; i++) {
+            int num = Integer.parseInt(arr[i]);
+
+            if(num < min) {
+                min = num;
+            }
+
+            if(num > max) {
+                max = num;
+            }
+        }
+
+        return min + " " + max;
+    }
+}
