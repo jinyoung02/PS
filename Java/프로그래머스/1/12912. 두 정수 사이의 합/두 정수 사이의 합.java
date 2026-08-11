20cd1f27b3584765d69bd55a3dae5d1622bad4f2
JinYoung Kim <jink8962@naver.com>
2026-08-11 22:06:43 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 11.05 ms, Memory: 88.7 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.java
@@ -0,0 +1,21 @@
+class Solution {
+    public long solution(int a, int b) {
+        long sum = 0;
+        
+        if(a<b) {
+            for(int i=a; i<=b; i++) {
+                sum = sum + i;
+            }
+        }
+        else if(a>b) {
+            for(int i=b; i<=a; i++) {
+                sum = sum + i;
+            }
+        }
+        else {
+            return a;
+        }
+        
+        return sum;
+    }
+}
